86729a00c66d4ff0753fbc22d5588a51d2b67421
kzoeps <kzoepa@gmail.com>
2022-04-28 20:25:57 +0600

    :tada:

---
+++ docusaurus.config.js
@@ -0,0 +1,71 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Chakra Formik',
+  tagline: 'Easily bind your chakra-ui forms with Formik',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'hacket', // Usually your GitHub org/user name.
+  projectName: 'chakra-formik', // Usually your repo name.
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl: 'https://github.com/kzoeps/chakra-formik',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'Chakra Formik',
+        logo: {
+          alt: 'Chakra Formik',
+          src: 'img/chakra-formik.png',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'Tutorial',
+          },
+          {to: '/blog', label: 'Blog', position: 'left'},
+          {
+            href: 'https://github.com/facebook/docusaurus',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        copyright: `Copyright © ${new Date().getFullYear()} Hacket, Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
---
+++ src/components/HomepageFeatures/index.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import clsx from 'clsx';
+import styles from './styles.module.css';
+import Link from "@docusaurus/core/lib/client/exports/Link";
+
+const FeatureList = [
+  {
+    title: 'Easy to Use',
+    description: (
+      <>
+        Quickly bind formik with chakra forms. No more setting up MyAppInput again and again
+      </>
+    ),
+  },
+  {
+    title: 'Chakra UI',
+	  link: 'https://chakra-ui.com/',
+    description: (
+      <>
+		  Chakra UI is a simple, modular and accessible component library that gives you the building blocks you need to build your React applications
+      </>
+    ),
+  },
+  {
+    title: 'Formik',
+	  link: 'https://formik.org/',
+    description: (
+      <>
+		  Build forms in React, without the tears.
+	  </>
+    ),
+  },
+];
+
+const Title = (title) => <h3>{title}</h3>
+function Feature({link = undefined, title, description}) {
+  return (
+    <div className={clsx('col col--4')}>
+      <div className="text--center padding-horiz--md">
+		  {link ? <Link target="_blank" to={link} className={styles.featureTitle} rel="noreferrer">{title}</Link> : <span className={styles.featureTitle}>{title}</span>}
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default function HomepageFeatures() {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
